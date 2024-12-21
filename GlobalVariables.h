b5217ccce1e003707084cf442204b67cefb80165
klin-h <2353833@tongji.edu.cn>
2024-12-22 01:46:16 +0800

    Update GlobalVariables.h

--- GlobalVariables.h
+++ GlobalVariables.h
@@ -3,9 +3,11 @@
 
 #include "cocos2d.h"
 
-extern cocos2d::Scene* g_sharedScene; // 声明一个全局场景指针
+extern cocos2d::Scene* g_sharedScene; // 澹版槑涓�涓叏灞�鍦烘櫙鎸囬拡
 extern cocos2d::TMXTiledMap* g_sharedTMXone;
 extern cocos2d::TMXTiledMap* g_sharedTMXtwo;
 extern cocos2d::TMXTiledMap* g_sharedTMXthree;
 extern cocos2d::TMXTiledMap* g_sharedTMXcurrent;
+extern int g_time;
+extern cocos2d::TMXTiledMap* g_sharedTMXfour;
 #endif // GLOBALVARIABLES_H
