c35977f228fd4cb4f73b39f9819954bc18d98eba
klin-h <2353833@tongji.edu.cn>
2024-12-23 00:38:54 +0800

    最终版

--- PlantingSystem.h
+++ PlantingSystem.h
@@ -1,30 +1,22 @@
-//#pragma once
-//#ifndef PLANTING_SYSTEM_H
-//#define PLANTING_SYSTEM_H
-//
-//#include "cocos2d.h"
-//#include "Crop.h"
-//#include <vector>
-//#include "Land.h"
-//class PlantingSystem : public cocos2d::Node {
-//public:
-//    // 创建种植系统
-//    CREATE_FUNC(PlantingSystem);
-//
-//    // 初始化
-//    virtual bool init() override;
-//
-//    // 种植作物
-//    bool plantCrop(const std::string& cropName, float growthTime, const std::string& spriteFile, const cocos2d::Vec2& position);
-//
-//    // 在每帧更新作物状态
-//    void update(float deltaTime) override;
-//
-//private:
-//    // 检查位置是否合法
-//    bool isPositionValid(const cocos2d::Vec2& position);
-//
-//    std::vector<Crop*> crops; // 存储已种植的作物
-//};
-//
-//#endif 
+#ifndef __PLANTING_SYSTEM_H__
+#define __PLANTING_SYSTEM_H__
+
+#include "cocos2d.h"
+#include "Crop.h"
+#include <vector>
+
+class PlantingSystem : public cocos2d::Node {
+public:
+    virtual bool init();
+
+    bool plantCrop(const std::string& cropName, float growthTime, const std::string& spriteFileconst, const cocos2d::Vec2& position);
+    void harvestCrop(cocos2d::Vec2 touchPosition);
+    void update(float deltaTime);
+
+    CREATE_FUNC(PlantingSystem);
+
+private:
+    std::vector<Crop*> crops; // 存储作物的容器
+};
+
+#endif // __PLANTING_SYSTEM_H__
