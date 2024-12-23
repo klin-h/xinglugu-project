6a2b404608448e3cce3ad3c4e30caec50d9ba2b3
piggyisapiggy <1218629565@qq.com>
2024-12-23 12:56:35 +0800

    Update and rename TaskList.h to 角色/TaskList.h

--- TaskList.h
+++
@@ -1,9 +0,0 @@
-#include "cocos2d.h"
-
-// 任务结构体
-struct Task {
-    std::string taskDescription;  // 任务描述
-    int targetItemCount;          // 目标收集物品数量
-    int rewardDescription; // 任务奖励描述
-    bool completed;               // 任务是否完成
-};
