25cab3b8f9e0c2dde429824fdde5b0d4f4329bbe
Ty <2353833@tongji.edu.cn>
2025-12-16 15:54:56 +0800

    Add files via upload

---
+++ 动物功能/AnimalFactory.cpp
@@ -0,0 +1,43 @@
+#include "AnimalFactory.h"
+
+Animal* AnimalFactory::create(AnimalType type) {
+    switch (type) {
+        case AnimalType::Cow:
+            return Cow::create();
+        case AnimalType::Pig:
+            return Pig::create();
+        case AnimalType::Sheep:
+            return Sheep::create();
+        case AnimalType::Rabbit:
+            return Rabbit::create();
+        case AnimalType::Chicken:
+            return Chicken::create();
+        case AnimalType::Dog:
+            return Dog::create();
+        default:
+            return nullptr;
+    }
+}
+
+Animal* AnimalFactory::createById(const std::string& id) {
+    if (id == "cow") {
+        return create(AnimalType::Cow);
+    }
+    if (id == "pig") {
+        return create(AnimalType::Pig);
+    }
+    if (id == "sheep") {
+        return create(AnimalType::Sheep);
+    }
+    if (id == "rabbit") {
+        return create(AnimalType::Rabbit);
+    }
+    if (id == "chicken") {
+        return create(AnimalType::Chicken);
+    }
+    if (id == "dog") {
+        return create(AnimalType::Dog);
+    }
+    return nullptr;
+}
+
---
+++ 动物功能/AnimalFactory.h
@@ -0,0 +1,31 @@
+#ifndef ANIMAL_FACTORY_H
+#define ANIMAL_FACTORY_H
+
+#include "Animal.h"
+#include "Cow.h"
+#include "Pig.h"
+#include "Chicken.h"
+
+#include <string>
+
+enum class AnimalType {
+    Cow,
+    Pig,
+    Sheep,
+    Rabbit,
+    Chicken,
+    Dog,
+};
+
+class AnimalFactory {
+public:
+    static Animal* create(AnimalType type);
+    static Animal* createById(const std::string& id);
+private:
+    AnimalFactory() = default;
+    AnimalFactory(const AnimalFactory&) = delete;
+    AnimalFactory& operator=(const AnimalFactory&) = delete;
+};
+
+#endif // ANIMAL_FACTORY_H
+
